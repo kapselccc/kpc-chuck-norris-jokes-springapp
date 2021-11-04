3165a40bd99d97693c66be632bb58e0ac0eb1ee5
kapselccc <s184848@student.pg.edu.pl>
2021-11-04 15:26:16 +0100

    Template

---
+++ src/main/java/kpc/springframework/chucknorisjokes/ChuckNorisJokesApplication.java
@@ -0,0 +1,14 @@
+package kpc.springframework.chucknorisjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorisJokesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChuckNorisJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kpc/springframework/chucknorisjokes/ChuckNorisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package kpc.springframework.chucknorisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
